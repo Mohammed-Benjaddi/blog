d379c4416d866516ff9a81820ef5946ffad0c66c
Simopro77 <simopro665@gmail.com>
2023-07-06 19:40:14 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   serialize: (value) => {
     if (typeof value === 'object' && value !== null) {
       return JSON.stringify(value);
